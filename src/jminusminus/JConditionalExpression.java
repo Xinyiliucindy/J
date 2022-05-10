862268bfc299ee7f5a7345b7fbabbec8affdd8a9
ZhidieWu <93614558+ZhidieWu@users.noreply.github.com>
2022-05-10 20:27:04 +0200

    Revert "forstatement"
    This reverts commit c574f625a82d5917c69deea21ab9c6929c6b8c7d.

--- src/jminusminus/JConditionalExpression.java
+++ src/jminusminus/JConditionalExpression.java
@@ -1,74 +1,28 @@
 package jminusminus;
 import static jminusminus.CLConstants.*;
 public class JConditionalExpression extends JExpression {
-    private JExpression condition;
+    private JExpression rhs;
 
-    private JExpression firstpart;
-    private JExpression secondpart;
+    private JExpression condition;
+    private JExpression assign;
 
     /**
      * Constructs an AST node for a conditional expression.
      *
      * @param line      line in which the conditional expression occurs in the source file.
-     * @param condition test expression.
-     * @param firstpart
-     * @param secondpart
+     * @param rhs test expression.
+     * @param condition
+     * @param assign  
      */
-    public JConditionalExpression(int line, JExpression condition, JExpression firstpart,
-                                  JExpression secondpart) {
+    public JConditionalExpression(int line, JExpression rhs, JExpression condition,
+                                  JExpression assign) {
         super(line);
+        this.rhs = rhs;
         this.condition = condition;
-        this.firstpart = firstpart;
-        this.secondpart = secondpart;
+        this.assign = assign;
         
     }
-    public JExpression analyze(Context context){ 
-        condition = (JExpression) condition.analyze(context);
-        firstpart = (JExpression) firstpart.analyze(context);
-        secondpart = (JExpression) secondpart.analyze(context);
-        
-        condition.type().mustMatchExpected(line(), Type.BOOLEAN);
-        secondpart.type().mustMatchExpected(line(), firstpart.type());
-        type = firstpart.type();    
-        return this;}
-    public void codegen(CLEmitter output){
-        String thenLabel = output.createLabel();
-        String elseLabel = output.createLabel();
-
-        condition.codegen(output, thenLabel, false);
-
-        condition.codegen(output);
-
-        if (firstpart != null) {
-            output.addBranchInstruction(GOTO, elseLabel);
-        }
-
-        output.addLabel(thenLabel);
-        
-        if (secondpart != null) {
-            secondpart.codegen(output);
-            output.addLabel(elseLabel);
-        }
-    }
-    public void writeToStdOut(PrettyPrinter p) {
-        p.printf("<JConditionalExpression line=\"%d\">\n", line());
-        p.indentRight();
-        p.printf("<First>\n");
-        p.indentRight();
-        condition.writeToStdOut(p);
-        p.indentLeft();
-        p.printf("</First>\n");
-        p.printf("<Second>\n");
-        p.indentRight();
-        firstpart.writeToStdOut(p);
-        p.indentLeft();
-        p.printf("</Second>\n");
-        p.printf("<Third>\n");
-        p.indentRight();
-        secondpart.writeToStdOut(p);
-        p.indentLeft();
-        p.printf("</Third>\n");
-        p.indentLeft();
-        p.printf("</JConditionalExpression>\n");
-    }
+    public JExpression analyze(Context context){ return this;}
+    public void codegen(CLEmitter output){}
+    public void writeToStdOut(PrettyPrinter p) {}
 }
